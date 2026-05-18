d591163061db2249a9fad1b64f476380b1b9ea0c
imtiyaz-deepcoder <imtiyaz@deepcoder.dev>
2026-05-18 15:38:36 +0530

    example-1

--- MyUnitTestingApp/src/main/java/com/service/Demo.java
+++ MyUnitTestingApp/src/main/java/com/service/Demo.java
@@ -1,4 +1,35 @@
 package com.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Demo {
+
+    public int sum(int x, int y){
+        return x+y;
+    }
+
+    public String computeGrade(List<Double> list){
+        //validate input
+        list.forEach(mark-> {
+            if(mark > 100) throw new RuntimeException("Subject Marks > 100 detected.");
+            if(mark < 0 ) throw new RuntimeException("Subject Marks < 0 detected.");
+        });
+
+        // compute total marks
+         double totalMarks = list.size()* 100;
+
+        // compute percent
+        // double marksSecured = list.stream().collect(Collectors.summingDouble((m)->m)).doubleValue();
+        double marksSecured = list.stream().mapToDouble((m)-> m).sum();
+        double percent = (marksSecured / totalMarks) * 100;
+
+        // compute grade
+        if(percent > 75)
+            return "A";
+        if(percent > 60)
+            return "B";
+
+        return "C";
+    }
 }
--- MyUnitTestingApp/src/test/java/com/service/DemoTest.java
+++ MyUnitTestingApp/src/test/java/com/service/DemoTest.java
@@ -1,4 +1,39 @@
 package com.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class DemoTest {
+    Demo demo = new Demo();
+    @Test
+    public void sumTest(){
+        Assertions.assertEquals(5, demo.sum(2,3));
+        Assertions.assertEquals(1, demo.sum(-2,3));
+        Assertions.assertEquals(-5, demo.sum(-2,-3));
+        Assertions.assertEquals(0, demo.sum(0,0));
+    }
+
+    @Test
+    public void computeGradeTest(){
+        // prepare the input
+        List<Double> student1 = List.of(96d,94.5d,78d,85d);
+        List<Double> student2 = List.of(96d,94.5d,178d,85d);
+        List<Double> student3 = List.of(96d,94.5d,78d,-15d);
+        List<Double> student4 = List.of(66d,64.5d,78d,60d);
+        List<Double> student5 = List.of(56d,64.5d,58d,50d);
+        // Use Case : Grade A
+        Assertions.assertEquals("A", demo.computeGrade(student1));
+        // Use Case : Grade B
+        Assertions.assertEquals("B", demo.computeGrade(student4));
+        // Use Case : Grade C
+        Assertions.assertEquals("C", demo.computeGrade(student5));
+        // Use Case : Marks > 100 exception
+        RuntimeException e =  Assertions.assertThrows(RuntimeException.class, ()->demo.computeGrade(student2));
+        Assertions.assertEquals("Subject Marks > 100 detected.", e.getMessage());
+        // Use Case : negative exception
+        RuntimeException e1 =  Assertions.assertThrows(RuntimeException.class, ()->demo.computeGrade(student3));
+        Assertions.assertEquals("Subject Marks < 0 detected.", e1.getMessage());
+    } //JIRA 3345
 }
