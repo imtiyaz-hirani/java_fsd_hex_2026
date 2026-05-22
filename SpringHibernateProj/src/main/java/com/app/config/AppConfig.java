2e86adc2092bb1550e0f36ae7a823c927361e701
imtiyaz-deepcoder <imtiyaz@deepcoder.dev>
2026-05-22 10:32:16 +0530

    spring-hirnate-setup

---
+++ SpringHibernateProj/src/main/java/com/app/config/AppConfig.java
@@ -0,0 +1,48 @@
+package com.app.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "com.app")
+@EnableTransactionManagement //<- tells spring that it need to manage transaction
+public class AppConfig {
+
+    @Bean
+    public DataSource getDBConnection(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        // url, username, password, driver class
+        dataSource.setUrl("jdbc:mysql://localhost:3306/crime_db");
+        dataSource.setUsername("root");
+        dataSource.setPassword("deepcoder");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean getEntityManagerFactory(DataSource dataSource){
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setPackagesToScan("com.app.model"); //<- instead of individual class we give package only once
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter()); //<- jpa need this
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+
+        // Attach the properties to emf
+
+        emf.setJpaProperties(properties);
+        return emf;
+
+    }
+}
