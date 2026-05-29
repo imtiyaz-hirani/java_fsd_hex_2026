87aecf821de7f2967d2a68d254c7fd00b2978bb6
imtiyaz-deepcoder <imtiyaz@deepcoder.dev>
2026-05-29 10:41:11 +0530

    Security Filter Chain added

--- cms/cms/src/main/java/com/cms/config/SecurityConfig.java
+++ cms/cms/src/main/java/com/cms/config/SecurityConfig.java
@@ -2,12 +2,18 @@ package com.cms.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -24,4 +30,19 @@ public class SecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(user1, user2);
     }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) /// Spring needs this for POST,PUT & DELETE
+                //.csrf(ref->ref.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                                .anyRequest().authenticated()
+                       // .anyRequest().permitAll() -- all API work without Auth
+                );
+        http.httpBasic(Customizer.withDefaults()); //i am telling Spring that i am using Basic Auth technique
+        return http.build();
+    }
+
 }
