62eae2eb335b0542e1d0fff733aa991012bfae1d
imtiyaz-deepcoder <imtiyaz@deepcoder.dev>
2026-06-06 14:54:03 +0530

    inital settings

---
+++ MyReactApp/src/App.jsx
@@ -0,0 +1,12 @@
+function App(){  // This is a react component
+ 
+
+ return ( // Every react component, must return a JSX : HTML + CSS + Javascript 
+  <div>
+      <h1>Hello React</h1>
+  </div>
+ )
+
+}
+
+export default App; 
