a550c34db20c8fe3242cf05d2e242a8c83e70915
imtiyaz-deepcoder <imtiyaz@deepcoder.dev>
2026-06-08 10:27:10 +0530

    Array ops - filter , sort + spread operator

--- MyReactApp/src/components/Products.jsx
+++ MyReactApp/src/components/Products.jsx
@@ -2,11 +2,36 @@ import { useState } from "react"
 import { products } from "../sample data/products"
 function Products() {
 
+    const [productData, setProductData] = useState(products)
+
+    const SortByPrice = (direction)=>{
+        
+        if(direction === 'ASC')
+           setProductData( [...productData].sort((p1,p2)=> p1.price - p2.price))
+        if(direction === 'DESC')
+           setProductData( [...productData].sort((p1,p2)=> p2.price - p1.price))
+    }
+
+    const FilterInStock = ()=>{
+        setProductData([...productData].filter(p=>p.inStock === true))
+    }
+
+    const reset=()=>{
+        setProductData(products)
+    }
     return (
         <div>
             <h1>List of Products</h1>
+            <div>
+                <p>
+                    <button onClick={()=>SortByPrice('ASC')}>Sort by Price: ASC</button>
+                    <button onClick={()=>SortByPrice('DESC')}>Sort by Price: DESC</button>
+                    <button onClick={()=>FilterInStock()}>Show products in Stock</button>
+                    <button onClick={()=>reset()}>Reset</button>
+                </p>
+            </div>
             <table>
-
+                <thead>
                 <tr>
                     <td>Sr. No</td>
                     <td>Name</td>
@@ -16,11 +41,11 @@ function Products() {
                     <td>In Stock</td>
                     <td> </td>
                 </tr>
-
+                </thead>
                 <tbody>
                     {
-                        products.map((p, index) => (
-                            <tr>
+                        productData.map((p, index) => (
+                            <tr key ={index}>
                                 <td>{index + 1}</td>
                                 <td>{p.name}</td>
                                 <td>{p.brand}</td>
